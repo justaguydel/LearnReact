f31b46c87b3a07ca85132e6d86ccbd88bab3d655
KAM\student <student>
2024-07-07 15:41:15 +0300

    First version

---
+++ LearnReact/src/Components/Body/Body.jsx
@@ -0,0 +1,8 @@
+export default function Body() {
+   
+    return (
+        <div>
+            <h1>Im Body!</h1>
+        </div>        
+    )
+  }
---
+++ LearnReact/src/Components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+   
+    return (
+        <div>
+            <h1>Im Footer!</h1>
+        </div>        
+    )
+  }
